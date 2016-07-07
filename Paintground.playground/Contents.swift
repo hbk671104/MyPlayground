8f105fb8ec0854a13a2d6b1bc8de7932983e83e8
hbk671104 <hbk671104@gmail.com>
2016-07-07 15:27:52 +0800

    find nearest point index

--- Paintground.playground/Contents.swift
+++ Paintground.playground/Contents.swift
@@ -3,6 +3,35 @@
 import UIKit
 import XCPlayground
 
+struct GraphUtil {
+	
+	private static func distanceBetweenPoints(point1: CGPoint, point2: CGPoint) -> CGFloat {
+		return sqrt(pow(point1.x-point2.x, 2)+pow(point1.y-point2.y, 2))
+	}
+	
+	static func nearestDefinedPointIndices(touchedPoint: CGPoint, definedPoints: [[UIBezierPath]]) -> (dimension: Int, score: Int) {
+		var shortestDistance: CGFloat = 0
+		var dimensionIndex: Int = 0
+		var scoreIndex: Int = 0
+		for dimension in definedPoints {
+			for score in dimension {
+				if shortestDistance != 0 {
+					let distance = distanceBetweenPoints(score.currentPoint, point2: touchedPoint)
+					if distance < shortestDistance {
+						shortestDistance = distance
+						dimensionIndex = definedPoints.indexOf({$0 == dimension})!
+						scoreIndex = dimension.indexOf(score)!
+					}
+				} else {
+					shortestDistance = distanceBetweenPoints(score.currentPoint, point2: touchedPoint)
+				}
+			}
+		}
+		return (dimensionIndex, scoreIndex)
+	}
+	
+}
+
 class InteractiveGraphView: UIView {
 	
     /// Dimensions
@@ -10,6 +39,9 @@ class InteractiveGraphView: UIView {
 	
 	/// Max score allowed
 	var maxScore: Int = 5
+	
+	/// Selected score
+	var selectedScore: [[Int]] = []
 	
 	/// Underlying graph stroke and fill color
 	var underlyingGraphViewColor: UIColor = UIColor.blackColor()
@@ -25,6 +57,8 @@ class InteractiveGraphView: UIView {
 	
 	init(origin: CGPoint, sideLength: CGFloat) {
 		super.init(frame: CGRectMake(origin.x, origin.y, sideLength, sideLength))
+		
+		// View setup
 		backgroundColor = UIColor.whiteColor()
 		centerPoint = CGPointMake(frame.size.height/2, frame.size.width/2)
 		innerCircleRadius = sideLength*0.4/CGFloat(maxScore)
@@ -43,30 +77,39 @@ class InteractiveGraphView: UIView {
 		for i in 0..<dimensions.count {
 			let degree = piSegment*Double(i)
 			var scorePoints: [UIBezierPath] = []
-			for j in 0..<maxScore {
-				let radius = CGFloat(j+1)*innerCircleRadius
+			for j in 0...maxScore {
+				let radius = j != maxScore ? (CGFloat(j)+1)*innerCircleRadius : (CGFloat(j)+0.5)*innerCircleRadius
 				let x = radius*CGFloat(cos(degree))
 				let y = radius*CGFloat(sin(degree))
 				let point = CGPointMake(centerPoint.x+x, centerPoint.y+y)
 				
-				// Draw selection circle and connecting lines
-				let selectionCircle = UIBezierPath(arcCenter: point, radius: 5, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
-				selectionCircle.lineWidth = 2
-				selectionCircle.stroke()
-				scorePoints.append(selectionCircle)
-				if j == maxScore-1 {
-					let connectingLine = UIBezierPath()
-					connectingLine.lineWidth = 2
-					connectingLine.moveToPoint(centerPoint)
-					connectingLine.addLineToPoint(point)
-					connectingLine.stroke()
-				}
-				
-				// Draw connecting circle
-				if i == dimensions.count-1 {
-					let connectingCircle = UIBezierPath(arcCenter: centerPoint, radius: radius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
-					connectingCircle.lineWidth = 2
-					connectingCircle.stroke()
+				if j != maxScore {
+					// Draw selection circle and connecting lines
+					let selectionCircle = UIBezierPath(arcCenter: point, radius: 5, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+					selectionCircle.lineWidth = 2
+					selectionCircle.stroke()
+					scorePoints.append(selectionCircle)
+					if j == maxScore-1 {
+						let connectingLine = UIBezierPath()
+						connectingLine.lineWidth = 2
+						connectingLine.moveToPoint(centerPoint)
+						connectingLine.addLineToPoint(point)
+						connectingLine.stroke()
+					}
+					
+					// Draw connecting circle
+					if i == dimensions.count-1 {
+						let connectingCircle = UIBezierPath(arcCenter: centerPoint, radius: radius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+						connectingCircle.lineWidth = 2
+						connectingCircle.stroke()
+					}
+				} else {
+					// Add dimension name label
+					let label = UILabel(frame: CGRectMake(point.x-30, point.y-10, 60, 20))
+					label.textAlignment = .Center
+					label.font = UIFont.boldSystemFontOfSize(25.0)
+					label.text = dimensions[i]
+					self.addSubview(label)
 				}
 			}
 			definedPoints.append(scorePoints)
@@ -80,6 +123,12 @@ class InteractiveGraphView: UIView {
 			}
 		}
     }
+	
+	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		let touchPoint = touches.first?.locationInView(self)
+		let nearestIndices = GraphUtil.nearestDefinedPointIndices(touchPoint!, definedPoints: definedPoints)
+		print(nearestIndices)
+	}
     
 }
 
